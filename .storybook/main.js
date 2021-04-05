f1affff649a33b0136ad6580acd3aae6a3fafeb4
josimarrocha <rocha_josimar@yahoo.com.br>
2021-04-04 21:01:15 -0300

    Add Global Style

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,9 +1,14 @@
 module.exports = {
-  "stories": [
+  stories: [
     "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
+    "../src/stories/*.stories.@(js|jsx|ts|tsx)",
+    "../src/components/*.stories.@(js|jsx|ts|tsx)",
+    "../src/styles/*.stories.@(js|mdx)",
   ],
-  "addons": [
+  addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
+    "@storybook/preset-create-react-app",
     "@storybook/addon-knobs",
+  ],
+};
---
+++ src/styles/GlobalStyle.test.js
@@ -0,0 +1,8 @@
+import { render } from "@testing-library/react";
+import GlobalStyle from "./GlobalStyle";
+
+test("match snapshot", () => {
+  render(<GlobalStyle />);
+
+  expect(document.head).toMatchSnapshot();
+});
